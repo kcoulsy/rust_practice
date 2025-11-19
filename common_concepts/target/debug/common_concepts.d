824be2d7416685f9bd2696cabffc27a5d7cb5ec4
Kristian Coulson <kristiancoulson@gmail.com>
2025-11-19 13:01:47 +0000

    remove build from git

--- common_concepts/target/debug/common_concepts.d
+++
@@ -1,1 +0,0 @@
-C:\Users\krist\projects\rust_practice\common_concepts\target\debug\common_concepts.exe: C:\Users\krist\projects\rust_practice\common_concepts\src\main.rs
--- common_concepts/target/debug/deps/common_concepts-d53eb6051a78ba05.d
+++
@@ -1,5 +0,0 @@
-C:\Users\krist\projects\rust_practice\common_concepts\target\debug\deps\common_concepts-d53eb6051a78ba05.d: src\main.rs
-
-C:\Users\krist\projects\rust_practice\common_concepts\target\debug\deps\common_concepts-d53eb6051a78ba05.exe: src\main.rs
-
-src\main.rs:
